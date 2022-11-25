b63672f49b5f1e4f6fdbf7fd700862a91aa34fee
Eirik <eiriksnedland@gmail.com>
2022-11-25 12:48:52 +0100

    Tittel

--- bacit-dotnet.MVC.Tests/TestRepositories/TestSqlSuggestionsRepository.cs
+++ bacit-dotnet.MVC.Tests/TestRepositories/TestSqlSuggestionsRepository.cs
@@ -8,32 +8,32 @@ using System.Threading.Tasks;
 
 namespace bacit_dotnet.MVC.Tests.TestRepositories
 {
-    
+
     public class TestSqlSuggestionsRepository : ISuggestionRepository
     {
-        public void AddSuggestion(SuggestionEntity suggestion)
-        {
-            return;
-        }
-        public List<SuggestionEntity> GetSuggestions()
-        {
-            var suggestions = new List<SuggestionEntity>();
-            suggestions.Add(new SuggestionEntity { SuggestionID = 1 });
-            suggestions.Add(new SuggestionEntity { SuggestionID = 2 });
-            return suggestions;
-        }
-        public void Delete(int Suggestion)
-        {
-            return;
-        }
-        public void Edit(SuggestionEntity suggestion)
-        {
-            return;
-        }
+         public void AddSuggestion(SuggestionEntity suggestion)
+         {
+             return;
+         }
+         public List<SuggestionEntity> GetSuggestions()
+         {
+             var suggestions = new List<SuggestionEntity>();
+             suggestions.Add(new SuggestionEntity { SuggestionID = 1 });
+             suggestions.Add(new SuggestionEntity { SuggestionID = 2 });
+             return suggestions;
+         }
+         public void Delete(int Suggestion)
+         {
+             return;
+         }
+         public void Edit(SuggestionEntity suggestion)
+         {
+             return;
+         }
 
-        public List<SuggestionEntity> GetSuggestionsWithSearchQyery(string searchWord)
-        {
-            throw new NotImplementedException();
-        }
+         public List<SuggestionEntity> GetSuggestionsWithSearchQyery(string searchWord)
+         {
+             throw new NotImplementedException();
+         }
     }
 }
